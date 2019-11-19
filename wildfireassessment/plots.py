9865d927700764c4f0968214e0ea863a8dca33b6
Ai-Linh <aalten@ucmerced.edu>
2019-11-19 01:46:14 -0800

    adding new plot for confusion matrix

--- wildfireassessment/plots.py
+++ wildfireassessment/plots.py
@@ -10,6 +10,34 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, recall_score, precision_score, accuracy_score, f1_score
 from sklearn.utils.multiclass import unique_labels
 import numpy as np
+import seaborn as sns
+import pandas as pd
+
+#https://www.kaggle.com/agungor2/various-confusion-matrix-plots
+def plot_cm(y_true, y_pred, figsize=(10,10), classes=[], figname="results/test.png"):
+    cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=np.unique(y_true), columns=np.unique(y_true))
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(cm, cmap= "YlGnBu", annot=annot, fmt='', ax=ax, xticklabels=classes, yticklabels=classes)
+    plt.savefig(figname)
+    #plt.show()
 
 ## from scikit learn: https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
 def plot_confusion_matrix(y_true, y_pred, classes, normalize=False, title=None, cmap=plt.cm.Blues):
